23c162cab3da98833d3ce3a8bcd6e79282ca2896
fornet66 <fornet66@yahoo.com>
2015-06-18 00:24:22 +0800

    add app.ini for gogs

--- gogs_install.sh
+++ gogs_install.sh
@@ -1,4 +1,8 @@
 
 yum install golang
 wget https://github.com/gogits/gogs/releases/download/v0.6.1/linux_amd64.zip
+unzip linux_amd64.zip
+cp app.ini /home/git/gogs/custom/conf/app.ini
+gogs web
+
 
