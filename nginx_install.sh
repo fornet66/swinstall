bf35a6a73d18abe85a080e135a7a87b5a3c770aa
fornet66 <fornet66@yahoo.com>
2015-06-26 11:21:01 +0800

    haha

--- nginx_install.sh
+++ nginx_install.sh
@@ -3,9 +3,15 @@ yum install nginx
 cp nginx.conf /etc/nginx/nginx.conf
 systemctl start nging.service
 
+# 10.1.234.29
 firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
 	source address="10.1.234.30/24" \
 	port protocol="tcp" port="8080" accept"
+
+# 10.1.234.28
+firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
+	source address="10.1.234.30/24" \
+	port protocol="tcp" port="3000" accept"
 
 systemctl restart firewall-cmd
 
