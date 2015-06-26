3f7ad1e5a64ee4ee263cba55031e2c60aa84f7bb
fornet66 <fornet66@yahoo.com>
2015-06-26 11:12:19 +0800

    haha

--- nginx_install.sh
+++ nginx_install.sh
@@ -4,6 +4,8 @@ cp nginx.conf /etc/nginx/nginx.conf
 systemctl start nging.service
 
 firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
-	source address="10.1.234.29/24" \
+	source address="10.1.234.30/24" \
 	port protocol="tcp" port="8080" accept"
+
+systemctl restart firewall-cmd
 
