40fe60854627722d91b69dcd316dbe7f09858359
fornet66 <fornet66@yahoo.com>
2015-07-01 23:09:44 -0400

    nginx firewall

--- nginx_install.sh
+++ nginx_install.sh
@@ -13,5 +13,10 @@ firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
 	source address="10.1.234.30/24" \
 	port protocol="tcp" port="3000" accept"
 
+# 10.1.234.28 #svnadmin
+firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
+	source address="10.1.234.30/24" \
+	port protocol="tcp" port="80" accept"
+
 systemctl restart firewall-cmd
 
--- svn_install.sh
+++ svn_install.sh
@@ -3,4 +3,7 @@ yum install httpd
 yum install php
 yum install subversion
 yum install mod_dav_svn
+unzip svnadmin.zip
+mv svnadmin.zip /var/www/html
+chmod 777 /var/www/html/svnadmin/data
 
