94455d051119971bd2863802b1dbffa661ef2fae
fornet66 <fornet66@yahoo.com>
2015-07-01 23:07:01 -0400

    ports

--- nginx_install.sh
+++ nginx_install.sh
@@ -3,12 +3,12 @@ yum install nginx
 cp nginx.conf /etc/nginx/nginx.conf
 systemctl start nging.service
 
-# 10.1.234.29
+# 10.1.234.29 #ssm
 firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
 	source address="10.1.234.30/24" \
 	port protocol="tcp" port="8080" accept"
 
-# 10.1.234.28
+# 10.1.234.28 #gogs
 firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
 	source address="10.1.234.30/24" \
 	port protocol="tcp" port="3000" accept"
---
+++ svn_install.sh
@@ -0,0 +1,6 @@
+
+yum install httpd
+yum install php
+yum install subversion
+yum install mod_dav_svn
+
