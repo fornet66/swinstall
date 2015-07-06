f201e0550430dfaf7f486b382a0289d0596e1f1f
fornet66 <fornet66@yahoo.com>
2015-07-06 02:46:44 -0400

    nginx config modify

--- nginx_install.sh
+++ nginx_install.sh
@@ -14,10 +14,10 @@ firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
 	source address="10.1.234.30/24" \
 	port protocol="tcp" port="3000" accept"
 
-# for 10.1.234.28 #svnadmin
+# for 10.1.234.28 #scm manager
 firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
 	source address="10.1.234.30/24" \
-	port protocol="tcp" port="80" accept"
+	port protocol="tcp" port="8080" accept"
 
 systemctl restart firewall-cmd
 
