7afd3b6b5d162f5215df4f68e53067781177f98a
fornet66 <fornet66@yahoo.com>
2015-07-18 22:53:48 -0400

    ldap

--- ldap_install.sh
+++ ldap_install.sh
@@ -6,10 +6,12 @@ yum install openldap-clients
 yum install nss-pam-ldapd
 yum install nscd
 yum install nslcd
+systemctl start nscd
+systemctl start nslcd
 echo "session     required      pam_mkhomedir.so skel=/etc/skel/ umask=0077" >> /etc/pam.d/system-auth
 echo "session     optional      pam_ldap.so" >> /etc/pam.d/system-auth
 
-ldapsearch -h ldap.asiainfo.com -x -b 'ou=asiainfo-users,dc=ai,dc=com' -D 'ai\xienan' -w xienan
+ldapsearch -h ldap.asiainfo.com -x -b 'ou=asiainfo-users,dc=ai,dc=com' -LLL "(sAMAccountName=gaohn)" -D 'ai\xienan' -w xienan
 authconfig --enableldap --enableldapauth --ldapserver=ldap.asiainfo.com \
 	--ldapbasedn="ou=asiainfo-users,dc=ai,dc=com" --update
 
