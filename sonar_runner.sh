d575ffea726e1fa46258b1d7b2e38b068f4a26c4
fornet66 <fornet66@yahoo.com>
2015-07-22 15:14:50 -0400

    sonar

--- sonar_runner.sh
+++ sonar_runner.sh
@@ -1,14 +1,11 @@
 
 export SONAR_RUNNER_OPTS="-Xmx512m -XX:MaxPermSize=512m"
-sonar-runner -Dsonar.analysis.mode=incremental \
+sonar-runner -Dsonar.analysis.mode=preview \
 	-Dsonar.projectKey=upload \
 	-Dsonar.projectName=upload \
 	-Dsonar.projectVersion=1.0 \
 	-Dsonar.projectBaseDir=/home/xienan/work/products/ibs/bms/upload \
 	-Dsonar.sources=src \
-	-Dsonar.working.directory=/home/xienan/sonar_runner_work \
-	-Dsonar.issuesReport.html.enable=true \
-	-Dsonar.issuesReport.lightModeOnly \
-	-Dsonar.issuesReport.html.location=/home/xienan/sonar_runner_work \
-	-Dsonar.issuesReport.html.name=upload.html
+	-Dsonar.working.directory=/home/xienan/sonar_runner_work
+
 
