41bd51931bb25e80b562e2bdafe6a4b4e7d7667d
fornet66 <fornet66@yahoo.com>
2015-08-02 08:35:44 -0400

    sonar_runner

--- sonar/sonar_runner.sh
+++ sonar/sonar_runner.sh
@@ -1,11 +1,30 @@
 
 export SONAR_RUNNER_OPTS="-Xmx512m -XX:MaxPermSize=512m"
 sonar-runner -Dsonar.analysis.mode=preview \
-	-Dsonar.projectKey=upload \
-	-Dsonar.projectName=upload \
+	-Dsonar.projectKey=bms \
+	-Dsonar.projectName=bms \
 	-Dsonar.projectVersion=1.0 \
-	-Dsonar.projectBaseDir=/home/xienan/work/products/ibs/bms/upload \
-	-Dsonar.sources=src \
-	-Dsonar.working.directory=/home/xienan/sonar_runner_work
+	-Dsonar.projectBaseDir=/home/xienan/work/cvs/products/ibs \
+	-Dsonar.sources=. \
+	-Dsonar.working.directory=/home/xienan/temp/sonar_runner_work
+java -Ddepartment=010 -Dauthor=xienan -Dproject=bms -Dversion=2.0 -jar AnalysisReportplugin-1.0.jar
+
+sonar-runner -Dsonar.analysis.mode=preview \
+	-Dsonar.projectKey=skymanage \
+	-Dsonar.projectName=skymanage \
+	-Dsonar.projectVersion=1.0 \
+	-Dsonar.projectBaseDir=/home/xienan/work/cvs/products \
+	-Dsonar.sources=skymanage \
+	-Dsonar.working.directory=/home/xienan/temp/sonar_runner_work
+java -Ddepartment=010 -Dauthor=xienan -Dproject=skymanage -Dversion=2.0 -jar AnalysisReportplugin-1.0.jar
+
+sonar-runner -Dsonar.analysis.mode=preview \
+	-Dsonar.projectKey=np \
+	-Dsonar.projectName=np \
+	-Dsonar.projectVersion=3.0 \
+	-Dsonar.projectBaseDir=/home/xienan/work/cvs/products \
+	-Dsonar.sources=np \
+	-Dsonar.working.directory=/home/xienan/temp/sonar_runner_work
+java -Ddepartment=010 -Dauthor=xienan -Dproject=np -Dversion=2.0 -jar AnalysisReportplugin-1.0.jar
 
 
