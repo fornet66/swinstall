db1dce8afbc2c94800ea3b66ecc0f5556bedd70a
fornet66 <fornet66@yahoo.com>
2015-08-03 15:59:35 +0800

    ntp

--- ntp/ntp_install.sh
+++ ntp/ntp_install.sh
@@ -1,7 +1,8 @@
 
 yum install ntp -y
+yum install nmap -y
 tzselect
-#cp -f /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+cp -f /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 hwclock -w
 date -s 12/04/2014
 date -s 10:38
