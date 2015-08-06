e067e6ded2642eaead38a9695051a2463db6821a
fornet66 <fornet66@yahoo.com>
2015-08-06 15:01:48 +0800

    ssm

--- sonar/sonarqube_install.sh
+++ sonar/sonarqube_install.sh
@@ -3,6 +3,7 @@
 wget http://downloads.sonarsource.com/plugins/org/codehaus/sonar-plugins/sonar-ldap-plugin/1.4/sonar-ldap-plugin-1.4.jar
 wget http://downloads.sonarsource.com/plugins/org/codehaus/sonar-plugins/sonar-web-plugin/2.3/sonar-web-plugin-2.3.jar
 wget http://downloads.sonarsource.com/plugins/org/codehaus/sonar-plugins/javascript/sonar-javascript-plugin/2.6/sonar-javascript-plugin-2.6.jar
+wget http://downloads.sonarsource.com/plugins/org/codehaus/sonar-plugins/java/sonar-findbugs-plugin/3.2/sonar-findbugs-plugin-3.2.jar
 wget https://github.com/wenns/sonar-cxx/releases/download/v0.9.3/sonar-cxx-plugin-0.9.3.jar
 
 
