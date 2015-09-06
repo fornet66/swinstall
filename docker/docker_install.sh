791125ca717c1f7d8b8520db8e9bb58847098923
xienan <xienan@Xies-MBA.local>
2015-09-06 18:47:16 +0800

    docker nsenter command

--- docker/docker_install.sh
+++ docker/docker_install.sh
@@ -6,3 +6,8 @@ yum install -y device_mapper_libs
 
 docker run hello-world
 
+docker run -d --name mysql1 mysql
+PID=`docker  inspect  --format "{{ .State.Pid }}"  mysql1`
+nsenter --target $PID --mount --uts --ipc --net --pid
+docker attach `docker ps -q -a`
+
