cb9302be7b1618ad4bca5db8e6559fb58d7f22fc
xienan <xienan@Xies-MBA.local>
2015-09-09 10:21:46 +0800

    add viewvc ports

--- nginx/nginx_install.sh
+++ nginx/nginx_install.sh
@@ -19,5 +19,10 @@ firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
 	source address="10.1.234.30/24" \
 	port protocol="tcp" port="8080" accept"
 
+# for 10.1.234.28 #viewvc
+firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
+	source address="10.1.234.30/24" \
+	port protocol="tcp" port="8081" accept"
+
 systemctl restart firewall-cmd
 
