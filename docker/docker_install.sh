d61f506d74de0a83c18565fd44f48f929e0e4d12
xienan <xienan@Xies-MBA.local>
2015-09-09 11:07:53 +0800

    docker install device mapper

--- docker/docker_install.sh
+++ docker/docker_install.sh
@@ -2,7 +2,7 @@
 usermod -G wheel xienan
 wget -qO- https://get.docker.com/ | sh
 sudo usermod -aG docker xienan
-yum install -y device_mapper_libs
+yum install -y device_mapper
 
 docker run hello-world
 
