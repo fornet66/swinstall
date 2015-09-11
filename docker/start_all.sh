8c418077652a8b1fbb8bef5a01019acc26960ea5
xienan <xienan@Xies-MBA.local>
2015-09-11 18:18:42 +0800

    dockerfile

--- docker/start_all.sh
+++ docker/start_all.sh
@@ -8,7 +8,7 @@ docker -H 10.1.234.28:5555 run -d --name=mariadb -p 3306:3306 -v /mariadb:/maria
 # scm manager
 
 # sonarqube
-docker run -d --name=sonarqube --restart=on-failure:3 sonarqube
+docker -H 10.1.234.29:5555 run -d --name=sonarqube -p 9000:9000 --restart=on-failure:3 sonarqube
 # nginx
 docker run -d --name=nginx -p 80:80 --restart=on-failure:3 nginx
 
--- nginx/nginx_install.sh
+++ nginx/nginx_install.sh
@@ -4,11 +4,6 @@ yum install nginx
 cp nginx.conf /etc/nginx/nginx.conf
 systemctl start nging.service
 
-# for 10.1.234.29 #ssm
-firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
-	source address="10.1.234.30/24" \
-	port protocol="tcp" port="8080" accept"
-
 # for 10.1.234.28 #gogs
 firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
 	source address="10.1.234.30/24" \
@@ -24,6 +19,16 @@ firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
 	source address="10.1.234.30/24" \
 	port protocol="tcp" port="8081" accept"
 
+# for 10.1.234.29 #ssmtest
+firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
+	source address="10.1.234.30/24" \
+	port protocol="tcp" port="8080" accept"
+
+# for 10.1.234.29 #sonarqube
+firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
+	source address="10.1.234.30/24" \
+	port protocol="tcp" port="9000" accept"
+
 # for 10.1.234.30 # jenkins
 firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
 	source address="172.17.0.0/24" \
@@ -35,9 +40,9 @@ firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
 	port protocol="tcp" port="8081" accept"
 
 # for 10.1.234.30 # sonarqube
-firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
-	source address="172.17.0.0/24" \
-	port protocol="tcp" port="8082" accept"
+#firewall-cmd --permanent --zone=public --remove-rich-rule="rule family="ipv4" \
+#	source address="172.17.0.0/24" \
+#	port protocol="tcp" port="8082" accept"
 
 # for 10.1.234.30 # ssm
 firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
@@ -45,9 +50,9 @@ firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
 	port protocol="tcp" port="8083" accept"
 
 # for 10.1.234.30 # docsys
-firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
-	source address="172.17.0.0/24" \
-	port protocol="tcp" port="8085" accept"
+#firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
+#	source address="172.17.0.0/24" \
+#	port protocol="tcp" port="8085" accept"
 
 # for 10.1.234.30 # sonarcheck
 firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
