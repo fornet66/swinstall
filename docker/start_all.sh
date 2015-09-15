f07dfcbf10d14526784451f224017a5e77278be8
xienan <xienan@bogon.ailk.com>
2015-09-15 16:52:55 +0800

    add swarm

--- docker/start_all.sh
+++ docker/start_all.sh
@@ -1,14 +1,18 @@
 
-# gogs
-docker -H 10.1.234.28:5555 run -d --name=gogs -p 3000:3000 -v /git:/git --restart=on-failure:3 gogs
+docker run -d -p 2376:2375 --name swarm -v $(pwd)/cluster:/tmp/cluster swarm manage file:///tmp/cluster
+docker -H 127.0.0.1:2376 info
 
-# mariadb
-docker -H 10.1.234.28:5555 run -d --name=mariadb -p 3306:3306 -v /mariadb:/mariadb --restart=on-failure:3 mariadb
+docker -H 10.1.234.28:5555 restart gogs
+docker -H 10.1.234.28:5555 restart mariadb
+docker -H 10.1.234.28:5555 restart scm
+docker -H 10.1.234.29:5555 restart sonaruqbe
 
-# scm manager
-
-# sonarqube
-docker -H 10.1.234.29:5555 run -d --name=sonarqube -p 9000:9000 --restart=on-failure:3 sonarqube
 # nginx
 docker run -d --name=nginx -p 80:80 --restart=on-failure:3 nginx
+# gogs
+docker -H 10.1.234.28:5555 run -d --name=gogs -t -p 3000:3000 -v /etc/localtime:/etc/localtime:ro -v /git:/git --restart=on-failure:3 gogs
+# mariadb
+docker -H 10.1.234.28:5555 run -d --name=mariadb -t -p 3306:3306 -v /etc/localtime:/etc/localtime:ro -v /mariadb:/mariadb --restart=on-failure:3 mariadb
+# soanrqube
+docker -H 10.1.234.29:5555 run -d --name=sonarqube -t -p 9000:9000 -v /etc/localtime:/etc/localtime:ro --restart=on-failure:3 sonarqube
 
