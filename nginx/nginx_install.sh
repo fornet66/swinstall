c8aafbf48cb7b148f0dd652898d605976363da94
fornet66@yahoo.com <xienan@asiainfo.com>
2015-10-19 15:56:34 +0800

    add shipyard port

--- nginx/nginx_install.sh
+++ nginx/nginx_install.sh
@@ -39,6 +39,11 @@ firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
 	source address="10.1.234.30/24" \
 	port protocol="tcp" port="28017" accept"
 
+# for 10.1.234.30 # shipyard
+firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
+	source address="172.17.0.0/24" \
+	port protocol="tcp" port="8080" accept"
+
 # for 10.1.234.30 # ssm
 firewall-cmd --permanent --zone=public --add-rich-rule="rule family="ipv4" \
 	source address="172.17.0.0/24" \
