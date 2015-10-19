026363eb8959f7858795a602ba68d52b475a2950
fornet66@yahoo.com <xienan@asiainfo.com>
2015-10-19 15:46:04 +0800

    modify shipyard startup script

--- docker/start_all.sh
+++ docker/start_all.sh
@@ -1,30 +1,39 @@
 
-# docker daemon -H unix:///var/run/docker.sock -H tcp://0.0.0.0:5555 --group="docker" --graph="/docker"
+# docker daemon -H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375 --group="docker" --graph="/docker"
 
-docker -H 10.1.234.28:5555 restart scm
-docker -H 10.1.234.28:5555 restart gogs
-docker -H 10.1.234.28:5555 restart mariadb
-docker -H 10.1.234.29:5555 restart nexus
-docker -H 10.1.234.29:5555 restart jenkins
-docker -H 10.1.234.29:5555 restart sonaruqbe
-docker -H 10.1.234.30:5555 restart nginx
+docker -H 10.1.234.28:2375 restart scm
+docker -H 10.1.234.28:2375 restart gogs
+docker -H 10.1.234.28:2375 restart mariadb
+docker -H 10.1.234.29:2375 restart nexus
+docker -H 10.1.234.29:2375 restart jenkins
+docker -H 10.1.234.29:2375 restart sonaruqbe
+docker -H 10.1.234.29:2375 restart ansible
+docker -H 10.1.234.29:2375 restart nodejs
+docker -H 10.1.234.29:2375 restart robotframework
+docker -H 10.1.234.30:2375 restart nginx
 
 # scm
-docker -H 10.1.234.28:5555 run -d --name=scm -t -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -v /home/svnroot/scm-data:/home/svnroot/scm-data -v /svnroot:/svnroot --restart=on-failure:3 scm
+docker -H 10.1.234.28:2375 run -d --name=scm -ti -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -v /home/svnroot/scm-data:/home/svnroot/scm-data -v /svnroot:/svnroot --restart=on-failure:3 scm
 # gogs
-docker -H 10.1.234.28:5555 run -d --name=gogs -t -p 3000:3000 -v /etc/localtime:/etc/localtime:ro -v /git:/git --restart=on-failure:3 gogs
+docker -H 10.1.234.28:2375 run -d --name=gogs -ti -p 3000:3000 -v /etc/localtime:/etc/localtime:ro -v /git:/git --restart=on-failure:3 gogs
 # mariadb
-docker -H 10.1.234.28:5555 run -d --name=mariadb -t -p 3306:3306 -v /etc/localtime:/etc/localtime:ro -v /mariadb:/mariadb --restart=on-failure:3 mariadb
+docker -H 10.1.234.28:2375 run -d --name=mariadb -ti -p 3306:3306 -v /etc/localtime:/etc/localtime:ro -v /mariadb:/mariadb --restart=on-failure:3 mariadb
 # sonatype nexus
-docker -H 10.1.234.29:5555 run -d --name=nexus -t -p 8081:8081 -v /etc/localtime:/etc/localtime:ro -v /home/nexus/sonatype-work:/home/nexus/sonatype-work --restart=on-failure:3 nexus
+docker -H 10.1.234.29:2375 run -d --name=nexus -ti -p 8081:8081 -v /etc/localtime:/etc/localtime:ro -v /home/nexus/sonatype-work:/home/nexus/sonatype-work --restart=on-failure:3 nexus
 # jenkins
-docker -H 10.1.234.29:5555 run -d --name=jenkins -t -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -v /home/jenkins/jenkins-work:/home/jenkins/jenkins-work -v /home/jenkins/jenkins-backup:/home/jenkins/jenkins-backup --restart=on-failure:3 jenkins
+docker -H 10.1.234.29:2375 run -d --name=jenkins -ti -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -v /home/jenkins/jenkins-work:/home/jenkins/jenkins-work -v /home/jenkins/jenkins-backup:/home/jenkins/jenkins-backup --restart=on-failure:3 jenkins
 # soanrqube
-docker -H 10.1.234.29:5555 run -d --name=sonarqube -t -p 9000:9000 -v /etc/localtime:/etc/localtime:ro --restart=on-failure:3 sonarqube
+docker -H 10.1.234.29:2375 run -d --name=sonarqube -ti -p 9000:9000 -v /etc/localtime:/etc/localtime:ro --restart=on-failure:3 sonarqube
 # nginx
-docker run -d --name=nginx -p 80:80 --restart=always nginx
+docker -H 10.1.234.30:2375 run -d --name=nginx -ti -p 80:80 -v /etc/localtime:/etc/localtime:ro --restart=always nginx
+# rethinkdb
+docker run \
+	-ti \
+	-d \
+	--restart=always \
+	--name shipyard-rethinkdb \
+	rethinkdb
 # swarm
-docker run -d --name shipyard-swarm -v $(pwd)/cluster:/tmp/cluster swarm manage file:///tmp/cluster
 docker run \
 	-ti \
 	-d \
@@ -34,9 +43,7 @@ docker run \
 	--volumes-from=shipyard-certs \
 	-v $(pwd)/cluster:/tmp/cluster \
 	swarm:latest \
-	manage --host tcp://0.0.0.0:3375 proxy:2375 \
-	file:///tmp/cluster
-
+	manage --host tcp://0.0.0.0:3375 file:///tmp/cluster
 # shipyard
 docker run \
 	-ti \
@@ -49,7 +56,7 @@ docker run \
 	--volumes-from=shipyard-certs \
 	shipyard/shipyard:latest \
 	server \
-	--docker tcp://swarm:2375
+	--docker tcp://swarm:3375
 
 # mac
 sudo docker-machine start default
