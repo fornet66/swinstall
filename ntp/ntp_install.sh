21a0a70e2d3c98d7b23c8f9f5a07bdd4f268932c
xienan <xienan@yunjiacloud.com>
2016-07-30 12:58:05 +0800

    ntp

--- ntp/ntp_install.sh
+++ ntp/ntp_install.sh
@@ -1,4 +1,16 @@
 
+use pool found in http://www.pool.ntp.org/ as ntp.conf
+
+modify /etc/ntp.conf
+add following lines as public server
+server 0.us.pool.ntp.org
+server 1.us.pool.ntp.org
+server 2.us.pool.ntp.org
+server 3.us.pool.ntp.org
+
+add log config
+logfile /var/log/ntp.log
+
 yum install ntp -y
 yum install nmap -y
 tzselect
@@ -8,7 +20,9 @@ date -s 12/04/2014
 date -s 10:38
 ntpq -c version
 ntpq -p
-/usr/sbin/ntpdate 10.1.1.222
+
+client crontab add
+10 * * * * /usr/sbin/ntpdate 10.1.1.222
 
 # jvm use /etc/sysconfig/clock for time calc
 cat << EOF >> /etc/sysconfig/clock
