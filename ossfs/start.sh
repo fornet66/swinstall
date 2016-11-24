2f10f3a05ec4696d614f7a778fd679c5a8110dab
xienan <xienan@yunjiacloud.com>
2016-11-24 11:13:33 +0800

    crawler

---
+++ ossfs/start.sh
@@ -0,0 +1,3 @@
+
+ossfs oss-yunpan /owncloud -o passwd_file=./passwd-ossfs -o max_stat_cache_size=100000 -o enable_noobj_cache -o multireq_max=2000 -o parallel_count=100 -o multipart_size=100 -o url=http://oss-cn-hangzhou-iaas-d01-a.iaas.zj.pcc -ouid=33 -ogid=65534 -o allow_other
+
