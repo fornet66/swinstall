70831bf5b07c2ac1bbdd4548b5a29582e95fffc9
fornet66 <fornet66@yahoo.com>
2017-02-17 10:04:41 +0800

    docker start_all.sh

--- docker/start_all.sh
+++ docker/start_all.sh
@@ -13,29 +13,56 @@ docker -H 10.1.234.29:2375 restart robotframework
 docker -H 10.1.234.30:2375 restart nginx
 
 # redis
-docker run -d --name=redis -h redis -ti -p 6379:6379 -v /etc/localtime:/etc/localtime:ro \
-    -v /data/redis:/redis --restart=on-failure:3 redis /redis/6379.conf
+docker run -d --name=redis -h redis -ti -p 6379:6379 -h redis \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /data/redis:/redis \
+    --restart=on-failure:3 redis /redis/6379.conf
 # mongodb
 docker run -d --name=mongodb -h mongodb -ti -p 27017:27017 \
-    -v /etc/localtime:/etc/localtime:ro -v /data/mongodb:/data/db \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /data/mongodb:/data/db \
     --restart=on-failure:3 tutum/mongodb
 # owncloud
-docker run -d --name=owncloud -h owncloud -ti -v /etc/localtime:/etc/localtime:ro \
-    -v /data/owncloud:/var/www/html --restart=on-failure:3 owncloud
+docker run -d --name=owncloud -h owncloud -ti \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /data/owncloud:/var/www/html \
+    --restart=on-failure:3 owncloud
 # scm
-docker -H 10.1.234.28:2375 run -d --name=scm -ti -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -v /home/svnroot/scm-data:/home/svnroot/scm-data -v /svnroot:/svnroot --restart=on-failure:3 scm
+docker -H 10.1.234.28:2375 run -d --name=scm -ti -p 8080:8080 \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /home/svnroot/scm-data:/home/svnroot/scm-data \
+    -v /svnroot:/svnroot \
+    --restart=on-failure:3 scm
 # gogs
-docker -H 10.1.234.28:2375 run -d --name=gogs -ti -p 3000:3000 -v /etc/localtime:/etc/localtime:ro -v /git:/git --restart=on-failure:3 gogs
+docker -H 10.1.234.28:2375 run -d --name=gogs -h gogs -ti -p 3000:3000 \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /git:/git \
+    --restart=on-failure:3 gogs
 # mariadb
-docker -H 10.1.234.28:2375 run -d --name=mariadb -ti -p 3306:3306 -v /etc/localtime:/etc/localtime:ro -v /mariadb:/mariadb --restart=on-failure:3 mariadb
+docker -H 10.1.234.28:2375 run -d --name=mariadb -h mariadb -ti -p 3306:3306 \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /mariadb:/mariadb \
+    --restart=on-failure:3 mariadb
 # sonatype nexus
-docker -H 10.1.234.29:2375 run -d --name=nexus -ti -p 8081:8081 -v /etc/localtime:/etc/localtime:ro -v /home/nexus/sonatype-work:/home/nexus/sonatype-work --restart=on-failure:3 nexus
+docker -H 10.1.234.29:2375 run -d --name=nexus -h nexus -ti -p 8081:8081 \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /home/nexus/sonatype-work:/home/nexus/sonatype-work \
+    --restart=on-failure:3 nexus
 # jenkins
-docker -H 10.1.234.29:2375 run -d --name=jenkins -ti -p 8080:8080 -v /etc/localtime:/etc/localtime:ro -v /home/jenkins/jenkins-work:/home/jenkins/jenkins-work -v /home/jenkins/jenkins-backup:/home/jenkins/jenkins-backup --restart=on-failure:3 jenkins
+docker -H 10.1.234.29:2375 run -d --name=jenkins -h jenkins -ti -p 8080:8080 \
+    -u root -e "JENKINS_HOME=/jenkins/work" \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /data/jenkins/work:/jenkins/work \
+    -v /data/jenkins/backup:/jenkins/backup \
+    --restart=on-failure:3 jenkins
 # soanrqube
-docker -H 10.1.234.29:2375 run -d --name=sonarqube -ti -p 9000:9000 -v /etc/localtime:/etc/localtime:ro --restart=on-failure:3 sonarqube
+docker -H 10.1.234.29:2375 run -d --name=sonarqube -h sonarqube -ti -p 9000:9000 \
+    -v /etc/localtime:/etc/localtime:ro \
+    --restart=on-failure:3 sonarqube
 # nginx
-docker -H 10.1.234.30:2375 run -d --name=nginx -ti -p 80:80 -p 443:443 -v /etc/localtime:/etc/localtime:ro --restart=always nginx
+docker -H 10.1.234.30:2375 run -d --name=nginx -ti -h nginx -p 80:80 -p 443:443 \
+    -v /etc/localtime:/etc/localtime:ro \
+    --restart=always nginx
 # rethinkdb
 docker run \
 	-ti \
